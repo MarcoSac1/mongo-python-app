3c8e359101d8650d097ef0b3853a22e9cfe0bdf0
riccardo6178 <r.riccardo1210@gmail.com>
2026-07-02 12:42:28 +0200

    feature: user management

---
+++ app/routes/user_routes.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, HTTPException
+from pymongo.errors import DuplicateKeyError
+
+from app.repositories.user_repository import UserRepository 
+from app.models.user_model import UserCreate, UserUpdate
+
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+user_repository = UserRepository()
+
+@router.get("")
+def get_users():
+    users = user.user_repository.get_all()
+    return {
+        "count": len(users),
+        "data": users
+    }
+
+
+@router.get(f"/{user_id}")
+def get_user_by_id(user_id: str):
+    user = user_repository.get_by_id(user_id)
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="Utente non trovato")
+
+
+    return user
+
+@router.post("")
+def create_user(user: UserCreate):
+    try: 
+        user_data = user.model_dump()
+        return user_repository.create(user_data)
+    except DuplicateKeyError:
+        raise HTTPException(
+            status_code=400,
+            detail="Esiste gia un utente con questa mail"
+        )
+
+@router.put(f"/{user_id}")
+def update_user(user_id: str, user: UserUpdate):
+    user_data = user.model_dump(exclude_none=True)
+    updated_user = user_repository.update(user_id, user_data)
+
+    if updated_user is None:
+        raise HTTPException(status_code=404, detail="Utente non trovato")
+    return updated_user
+
+
+@router.get(f"/search/city/{city}")
+def search_user_by_city(city: str):
+    users = user_repository.search_by_city(city) 
+
+    return {
+        "count": len(users),
+        "data": users
+    }  
+
