88a7b33b27a0755476b9f2c106a72d848ce3ac9d
Marcosac1 <marcosaccone8@gmai.com>
2026-07-06 12:50:22 +0200

    Refactor UserRepository to ensure indexes are created only once and improve collection access

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -6,13 +6,21 @@ from app.repositories.base_repository import BaseRepository
 class UserRepository(BaseRepository):
     def __init__(self):
         super().__init__("users")
-        self.create_indexes()
+        self._indexes_created = False
 
-    def create_indexes(self):
-        self.collection.create_index(
-            [("email", ASCENDING)],
-            unique=True
-        )
+    @property
+    def collection(self):
+        collection = super().collection
+        self._ensure_indexes()
+        return collection
+
+    def _ensure_indexes(self):
+        if not self._indexes_created:
+            super().collection.create_index(
+                [("email", ASCENDING)],
+                unique=True
+            )
+            self._indexes_created = True
 
     def get_by_email(self, email: str):
         return self.find_one({"email": email})
